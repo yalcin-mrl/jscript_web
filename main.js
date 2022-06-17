2f387d346ff4df6e9fc1ce75f4034efc9defe152
yalcin-mrl <94745103+yalcin-mrl@users.noreply.github.com>
2022-06-17 13:25:00 +0300

    made function for day names

--- main.js
+++ main.js
@@ -5,5 +5,10 @@ let name = prompt("Adınız: ");
 let info = document.getElementById("name");
  info.innerHTML = ` Merhaba ${name}! Hoş geldin!`;
  let infoSaat = document.getElementById("saat");
- infoSaat.innerHTML = `${date.getHours()} : ${ date.getMinutes()} : ${date.getSeconds()}`;
+ infoSaat.innerHTML = `${date.getHours()} : ${ date.getMinutes()} : ${date.getSeconds()} ${getDayName(date.getDay())}`;
+}
+function getDayName(dayIndex){
+    var weekDays = new Array(7);
+    weekDays = ["Pazar","Pazartesi","Sali","Çarşamba","Perşembe","Cuma","Cumartesi"];
+    return weekDays[dayIndex];
 }
